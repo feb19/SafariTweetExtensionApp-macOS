e543abd03c47eb06a945b3abef60f9bc640ffa72
Nobuhiro Takahashi <nt@feb19.jp>
2018-07-03 22:48:30 +0900

    Initial Commit

---
+++ SafariExtensionApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SafariExtensionApp
+//
+//  Created by Nobuhiro Takahashi on 2018/07/03.
+//  Copyright © 2018年 Nobuhiro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
